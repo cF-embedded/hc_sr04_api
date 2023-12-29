e401a794a6dbca21a367f44716c6325215e236db
cF-embedded <filippocerta3@gmail.com>
2023-12-29 17:50:52 +0100

    project: add project

---
+++ src/hc_sr04/hc_sr04.c
@@ -0,0 +1,8 @@
+/******************************************************************************
+  *chip: any uC 
+  *brief: HC-SR04 driver
+  *details: Default configuration , initialize and most significant function
+  *author: cF-embedded.pl
+******************************************************************************/
+
+#include "hc_sr04.h"
---
+++ test/hc_sr04_tests/test.cpp
@@ -0,0 +1,11 @@
+/******************************************************************************
+  *brief: HC-SR04 driver TDD file
+  *author: cF-embedded.pl
+******************************************************************************/
+
+extern "C" {
+    #include "../../src/hc_sr04/hc_sr04.h"
+    #include "mocks/hc_sr04_hardware_mock.h"
+}
+
+#include <gtest/gtest.h>
