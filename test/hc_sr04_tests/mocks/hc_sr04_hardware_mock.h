a8383db1434541604997cf1a6cb0fa58451e2775
cF-embedded <filippocerta3@gmail.com>
2024-01-09 18:06:26 +0100

    project: add hc_sr04_hardware_mock

---
+++ test/hc_sr04_tests/mocks/hc_sr04_hardware_mock.h
@@ -0,0 +1,10 @@
+/******************************************************************************
+  *brief: HC-SR04 driver mocks for TDD header file
+  *author: cF-embedded.pl
+******************************************************************************/
+
+#ifndef __HC_SR04_HARDWARE_MOCK_H
+#define __HC_SR04_HARDWARE_MOCK_H
+
+
+#endif /* __HC_SR04_HARDWARE_MOCK_H */ 
