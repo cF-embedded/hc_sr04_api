26af8f3f2b3396152937d399cbfaa2ad0b539159
cF-embedded <filippocerta3@gmail.com>
2024-01-13 19:22:59 +0100

    hc_sr04_hardware: add hc_sr04_hardware_s_t

--- src/hc_sr04/hc_sr04_hardware.h
+++ src/hc_sr04/hc_sr04_hardware.h
@@ -8,4 +8,12 @@
 
 #include "hc_sr04_types.h"
 
+typedef struct
+{
+    void (*hc_sr04_hardware_init)(void);
+    void (*hc_sr04_hardware_tim_trig)(time_us_t);
+    time_us_t (*hc_sr04_hardware_tim_echo)(void);
+    hc_sr04_echo_e_t (*hc_sr04_hardware_wait_for_echo)(void);
+} hc_sr04_hardware_s_t;
+
 #endif /* __HC_SR04_HARDWARE_H */
