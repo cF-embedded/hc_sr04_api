55a1d53d59642b501334e69c018f3ebd417f02d3
cF-embedded <filippocerta3@gmail.com>
2024-01-13 19:23:20 +0100

    hc_sr04_types: add specific types for hc_sr04

--- src/hc_sr04/hc_sr04_types.h
+++ src/hc_sr04/hc_sr04_types.h
@@ -7,5 +7,24 @@
 #define __HC_SR04_TYPES_H
 
 #include "stdint.h"
+#include <stdbool.h>
+
+typedef float distance_cm_t;
+typedef uint16_t time_us_t;
+typedef uint16_t time_ms_t;
+
+typedef enum
+{
+    NON_READY,
+    WAIT_FOR_ECHO,
+    READY,
+    ERROR,
+} hc_sr04_state_e_t;
+
+typedef enum
+{
+    ECHO_OK,
+    ECHO_NOK,
+} hc_sr04_echo_e_t;
 
 #endif /* __HC_SR04_TYPES_H */
