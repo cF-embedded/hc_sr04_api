30abc6203a45e5b8f9d31e498f105fb4d4f5c69e
cF-embedded <filippocerta3@gmail.com>
2024-01-13 19:25:47 +0100

    hc_sr04: add struct and functions declaration

--- src/hc_sr04/hc_sr04.h
+++ src/hc_sr04/hc_sr04.h
@@ -10,5 +10,17 @@
 #include "hc_sr04_hardware.h"
 #include "hc_sr04_types.h"
 
+typedef struct
+{
+    time_us_t trig_time;
+    distance_cm_t last_distance;
+    hc_sr04_echo_e_t echo;
+    hc_sr04_state_e_t state;
+    hc_sr04_hardware_s_t hardware;
+} hc_sr04_s_t;
+
+void hc_sr04_init(hc_sr04_s_t*, hc_sr04_hardware_s_t);
+
+distance_cm_t hc_sr04_get_distance(hc_sr04_s_t*);
 
 #endif /* __HC_SR04_H */
