9c2b330e755a75aaa00f575ccc42b980fa9d94a6
cF-embedded <filippocerta3@gmail.com>
2024-01-14 09:11:26 +0100

    hc_sr04_tests: 1 test

--- test/hc_sr04_tests/test.cpp
+++ test/hc_sr04_tests/test.cpp
@@ -1,11 +1,23 @@
 /******************************************************************************
-  *brief: HC-SR04 driver TDD file
-  *author: cF-embedded.pl
-******************************************************************************/
+ *brief: HC-SR04 driver TDD file
+ *author: cF-embedded.pl
+ ******************************************************************************/
 
-extern "C" {
-    #include "../../src/hc_sr04/hc_sr04.h"
-    #include "mocks/hc_sr04_hardware_mock.h"
+extern "C"
+{
+#include "../../src/hc_sr04/hc_sr04.h"
+#include "mocks/hc_sr04_hardware_mock.h"
 }
 
 #include <gtest/gtest.h>
+
+TEST(hc_sr04_tests, status_eror_when_init_null_pointer)
+{
+    hc_sr04_s_t* hc_sr04 = NULL;
+    hc_sr04_hardware_s_t hardware_mock;
+    setup_hc_sr04_hardware_with_mocks(&hardware_mock);
+    
+    hc_sr04_status_e_t status = hc_sr04_init(hc_sr04, hardware_mock);
+
+    EXPECT_EQ(status, HC_SR04_STATUS_ERROR);
+}
