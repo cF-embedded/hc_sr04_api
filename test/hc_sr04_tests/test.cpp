0457bef7722b1d46a1ffa97a00e090d60eeea032
cF-embedded <filippocerta3@gmail.com>
2024-01-14 09:20:36 +0100

    hc_sr04_tests: 2 test

--- test/hc_sr04_tests/test.cpp
+++ test/hc_sr04_tests/test.cpp
@@ -11,13 +11,24 @@ extern "C"
 
 #include <gtest/gtest.h>
 
-TEST(hc_sr04_tests, status_eror_when_init_null_pointer)
+TEST(hc_sr04_tests, status_eror_after_init_null_pointer)
 {
     hc_sr04_s_t* hc_sr04 = NULL;
     hc_sr04_hardware_s_t hardware_mock;
     setup_hc_sr04_hardware_with_mocks(&hardware_mock);
-    
+
     hc_sr04_status_e_t status = hc_sr04_init(hc_sr04, hardware_mock);
 
     EXPECT_EQ(status, HC_SR04_STATUS_ERROR);
+}
+
+TEST(hc_sr04_tests, status_ok_after_init)
+{
+    hc_sr04_s_t hc_sr04;
+    hc_sr04_hardware_s_t hardware_mock;
+    setup_hc_sr04_hardware_with_mocks(&hardware_mock);
+
+    hc_sr04_status_e_t status = hc_sr04_init(&hc_sr04, hardware_mock);
+
+    EXPECT_EQ(status, HC_SR04_STATUS_OK);
 }
