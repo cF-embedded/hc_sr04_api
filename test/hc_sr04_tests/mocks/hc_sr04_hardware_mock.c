a1c6e6f1fbe3a32e85ab54788a4eec3605ea0291
cF-embedded <filippocerta3@gmail.com>
2024-01-14 09:11:02 +0100

    hc_sr04_hardware_mock: add mocks

--- test/hc_sr04_tests/mocks/hc_sr04_hardware_mock.c
+++ test/hc_sr04_tests/mocks/hc_sr04_hardware_mock.c
@@ -1,9 +1,42 @@
 /******************************************************************************
-  *brief: HC-SR04 driver mocks for TDD source file
-  *author: cF-embedded.pl
-******************************************************************************/
+ *brief: HC-SR04 driver mocks for TDD source file
+ *author: cF-embedded.pl
+ ******************************************************************************/
 
 #include "hc_sr04_hardware_mock.h"
 
+time_us_t mock_time;
 
+hc_sr04_echo_e_t mock_echo;
 
+void mock_hc_sr04_hardware_init(void) {}
+
+void mock_hc_sr04_hardware_tim_trig(time_us_t) {}
+
+time_us_t mock_hc_sr04_hardware_tim_echo(void)
+{
+    return mock_time;
+}
+
+hc_sr04_echo_e_t mock_hc_sr04_hardware_wait_for_echo(void)
+{
+    return mock_echo;
+}
+
+void mock_hc_sr04_hardware_set_wait_for_echo(hc_sr04_echo_e_t echo)
+{
+    mock_echo = echo;
+}
+
+void mock_hc_sr04_hardware_set_tim_echo(time_us_t time)
+{
+    mock_time = time;
+}
+
+void setup_hc_sr04_hardware_with_mocks(hc_sr04_hardware_s_t* hardware)
+{
+    hardware->hc_sr04_hardware_init = mock_hc_sr04_hardware_init;
+    hardware->hc_sr04_hardware_tim_trig = mock_hc_sr04_hardware_tim_trig;
+    hardware->hc_sr04_hardware_tim_echo = mock_hc_sr04_hardware_tim_echo;
+    hardware->hc_sr04_hardware_wait_for_echo = mock_hc_sr04_hardware_wait_for_echo;
+}
--- test/hc_sr04_tests/mocks/hc_sr04_hardware_mock.h
+++ test/hc_sr04_tests/mocks/hc_sr04_hardware_mock.h
@@ -1,10 +1,21 @@
 /******************************************************************************
-  *brief: HC-SR04 driver mocks for TDD header file
-  *author: cF-embedded.pl
-******************************************************************************/
+ *brief: HC-SR04 driver mocks for TDD header file
+ *author: cF-embedded.pl
+ ******************************************************************************/
 
 #ifndef __HC_SR04_HARDWARE_MOCK_H
 #define __HC_SR04_HARDWARE_MOCK_H
 
+#include "../../../src/hc_sr04/hc_sr04_hardware.h"
 
-#endif /* __HC_SR04_HARDWARE_MOCK_H */ 
+void mock_hc_sr04_hardware_init(void);
+void mock_hc_sr04_hardware_tim_trig(time_us_t);
+time_us_t mock_hc_sr04_hardware_tim_echo(void);
+hc_sr04_echo_e_t mock_hc_sr04_hardware_wait_for_echo(void);
+
+/* helper functions for mocks */
+void mock_hc_sr04_hardware_set_wait_for_echo(hc_sr04_echo_e_t);
+void mock_hc_sr04_hardware_set_tim_echo(time_us_t);
+void setup_hc_sr04_hardware_with_mocks(hc_sr04_hardware_s_t*);
+
+#endif /* __HC_SR04_HARDWARE_MOCK_H */
